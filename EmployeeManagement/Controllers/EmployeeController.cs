dd4993167a746121643b00449564c2659c1fc811
nitesh-khatri <khatrinitesh23@gmail.com>
2020-10-12 14:59:58 +0545

    Added Employees Details

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+public class EmployeeController : Controller
+{
+    public ActionResult Index()
+    {
+        var employees = Employee.GetEmployees();        
+        return View(employees);
+    }
+
+    public ActionResult Detail(string firstName)
+    {
+        var employees = Employee.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.FirstName == firstName);
+        
+        return View(employee);
+    }
+}
